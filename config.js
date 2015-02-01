6fddca17cc8cee90f935605dee32c0ddc6d613c3
tim walker <tim.twalker@gmail.com>
2015-01-31 20:14:17 -0800

    fresh jspm config

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 System.config({
   "paths": {
     "*": "*.js",
+    "es6-workflow-please/*": "app/*.js",
     "github:*": "jspm_packages/github/*.js",
     "npm:*": "jspm_packages/npm/*.js"
   }
@@ -8,120 +9,21 @@ System.config({
 
 System.config({
   "map": {
-    "mocha": "npm:mocha@^2.0.1",
-    "backbone": "npm:backbone@^1.1.2",
-    "lodash": "npm:lodash-node@^2.4.1",
-    "text": "github:systemjs/plugin-text@^0.0.2",
-    "jquery": "github:components/jquery@^2.1.1",
-    "mustache": "github:janl/mustache.js@^0.8.2",
-    "npm:mocha@2.0.1": {
-      "commander": "npm:commander@2.3.0",
-      "debug": "npm:debug@2.0.0",
-      "diff": "npm:diff@1.0.8",
-      "escape-string-regexp": "npm:escape-string-regexp@1.0.2",
-      "glob": "npm:glob@3.2.3",
-      "growl": "npm:growl@1.8.1",
-      "mkdirp": "npm:mkdirp@0.5.0",
-      "jade": "npm:jade@0.26.3"
+    "backbone": "npm:backbone@1.1.2",
+    "jquery": "github:components/jquery@2.1.3",
+    "lodash": "npm:lodash@3.0.1",
+    "mustache": "github:janl/mustache.js@1.0.0",
+    "text": "github:systemjs/plugin-text@0.0.2",
+    "github:jspm/nodelibs-process@0.1.0": {
+      "process": "npm:process@0.10.0"
     },
-    "npm:glob@3.2.3": {
-      "inherits": "npm:inherits@2",
-      "minimatch": "npm:minimatch@0",
-      "graceful-fs": "npm:graceful-fs@2.0",
-      "json": "npm:json@^9.0.1"
-    },
-    "npm:jade@0.26.3": {
-      "commander": "npm:commander@0.6.1",
-      "mkdirp": "npm:mkdirp@0.3.0"
-    },
-    "npm:debug@2.0.0": {
-      "ms": "npm:ms@0.6.2"
-    },
-    "npm:mkdirp@0.5.0": {
-      "minimist": "npm:minimist@0.0.8"
-    },
-    "npm:minimatch@0.4.0": {
-      "sigmund": "npm:sigmund@1.0",
-      "lru-cache": "npm:lru-cache@2"
-    },
-    "npm:commander@2.3.0": {},
-    "npm:escape-string-regexp@1.0.2": {},
-    "npm:diff@1.0.8": {},
-    "npm:growl@1.8.1": {},
-    "npm:inherits@2.0.1": {},
-    "npm:graceful-fs@2.0.3": {},
-    "npm:commander@0.6.1": {},
-    "npm:mkdirp@0.3.0": {},
-    "npm:ms@0.6.2": {},
-    "npm:minimist@0.0.8": {},
-    "npm:sigmund@1.0.0": {},
-    "npm:json@9.0.1": {},
-    "npm:lru-cache@2.5.0": {},
-    "github:jspm/nodelibs@0.0.5": {
-      "inherits": "npm:inherits@^2.0.1",
-      "pbkdf2-compat": "npm:pbkdf2-compat@^2.0.1",
-      "base64-js": "npm:base64-js@^0.0.4",
-      "ripemd160": "npm:ripemd160@^0.2.0",
-      "Base64": "npm:Base64@^0.2.0",
-      "ieee754": "npm:ieee754@^1.1.1",
-      "sha.js": "npm:sha.js@^2.2.6",
-      "json": "github:systemjs/plugin-json@^0.1.0"
-    },
-    "npm:ripemd160@0.2.0": {},
-    "npm:pbkdf2-compat@2.0.1": {},
-    "npm:base64-js@0.0.4": {},
-    "npm:Base64@0.2.1": {},
-    "npm:ieee754@1.1.4": {},
-    "npm:lodash-node@2.4.1": {},
     "npm:backbone@1.1.2": {
-      "underscore": "npm:lodash-node@^2.4.1"
+      "process": "github:jspm/nodelibs-process@0.1.0",
+      "underscore": "npm:lodash@3.0.1"
     },
-    "npm:underscore@1.7.0": {},
-    "npm:sha.js@2.2.6": {
-      "json": "npm:json@^9.0.1"
+    "npm:lodash@3.0.1": {
+      "process": "github:jspm/nodelibs-process@0.1.0"
     }
-  }
-});
-
-System.config({
-  "versions": {
-    "github:components/jquery": "2.1.1",
-    "npm:mocha": "2.0.1",
-    "npm:lodash-node": "2.4.1",
-    "npm:commander": [
-      "0.6.1",
-      "2.3.0"
-    ],
-    "npm:debug": "2.0.0",
-    "npm:diff": "1.0.8",
-    "npm:escape-string-regexp": "1.0.2",
-    "npm:glob": "3.2.3",
-    "npm:growl": "1.8.1",
-    "npm:mkdirp": [
-      "0.3.0",
-      "0.5.0"
-    ],
-    "npm:jade": "0.26.3",
-    "github:systemjs/plugin-text": "0.0.2",
-    "npm:inherits": "2.0.1",
-    "npm:minimatch": "0.4.0",
-    "npm:graceful-fs": "2.0.3",
-    "npm:json": "9.0.1",
-    "npm:ms": "0.6.2",
-    "github:janl/mustache.js": "0.8.2",
-    "npm:minimist": "0.0.8",
-    "npm:sigmund": "1.0.0",
-    "github:jspm/nodelibs": "0.0.5",
-    "npm:lru-cache": "2.5.0",
-    "npm:pbkdf2-compat": "2.0.1",
-    "npm:base64-js": "0.0.4",
-    "npm:ripemd160": "0.2.0",
-    "npm:Base64": "0.2.1",
-    "npm:ieee754": "1.1.4",
-    "npm:sha.js": "2.2.6",
-    "npm:backbone": "1.1.2",
-    "npm:underscore": "1.7.0",
-    "github:systemjs/plugin-json": "0.1.0"
   }
 });
 
